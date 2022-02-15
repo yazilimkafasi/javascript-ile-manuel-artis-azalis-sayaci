8341cd75c4888f8d9c63f5a9bc84afde16723f35
Yazılım Kafası <79590484+yazilimkafasi@users.noreply.github.com>
2022-02-15 11:54:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var input = document.getElementById("input")
+
+function increase() {
+    input.value = eval(input.value) + 1
+}
+
+function decrease() {
+    input.value = eval(input.value) - 1
+}
